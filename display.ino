f767767bc501be38791066b685337dd4c0e483c5
Konstantinos Adamis <kon.adamis2@gmail.com>
2025-02-28 14:27:51 +0200

    Update display.ino

--- display.ino
+++ display.ino
@@ -1,44 +1,41 @@
-// C++ code
-//
+// Arduino pins connected to the 7-segment display segments a to g, dp
+const int segmentPins[8] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-const int LED1 = 2;
-const int LED2 = 3;
-const int LED3 = 4;
-const int slideSwitch1 = 5;
-const int slideSwitch2 = 6;
+// Segment patterns for digits 0-9 (0babcdefg)
+const byte digitPatterns[10] = {
+    0b11111100,  // 0
+    0b01100000,  // 1
+    0b11011010,  // 2
+    0b11110010,  // 3
+    0b01100110,  // 4
+    0b10110110,  // 5
+    0b10111110,  // 6
+    0b11100000,  // 7
+    0b11111110,  // 8
+    0b11110110   // 9
+};
 
-void setup()
-{
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(slideSwitch1, INPUT);
-  pinMode(slideSwitch2, INPUT);
+void setup() {
+    // Set all segment pins as OUTPUT
+    for (int i = 0; i < 8; i++) {
+        pinMode(segmentPins[i], OUTPUT);
+    }
 }
 
-void loop()
-{
-  int slideSwitch1State = digitalRead(slideSwitch1);
-  int slideSwitch2State = digitalRead(slideSwitch2);
-  
-  if (slideSwitch1State ==  slideSwitch2State){
-    
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, LOW);
-  }
-  else if (slideSwitch1State > slideSwitch2State){
-    
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, HIGH);
-  }
-  else{
-    
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, LOW);
-    
-  }
-  
+void loop() {
+    // Loop through numbers 0-9
+    for (int number = 0; number < 10; number++) {
+        displayDigit(number);
+        delay(1000);  // Display the number for 1 second
+    }
+}
+
+// Function to display a single digit
+void displayDigit(int number) {
+    byte pattern = digitPatterns[number];
+
+    for (int i = 0; i < 8; i++) {
+        // Write HIGH or LOW to each segment based on the pattern
+        digitalWrite(segmentPins[i], bitRead(pattern, 7 - i) ? HIGH : LOW);
+    }
 }
